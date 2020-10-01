f3a84e79807dd268f33ac65e0b95bdc28378692d
Fialkaksu <fialkaksu1984@gmail.com>
2020-10-01 23:43:14 +0300

    feat: implemented min/max/avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length !== 0) {
+        return Math.min(...array);
+    }
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length !== 0) {
+        return Math.max(...array);
+    }
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length !== 0) {
+        let sum = 0;
+        array.forEach(el => sum += el);
+        return sum / array.length;
+    }
+    return 0;
 }
